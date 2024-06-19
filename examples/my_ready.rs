829d8d3e8fd9a4e3c71e18a7db0b3ea07fd5e35c
yikejason <18215513386@163.com>
2024-06-19 09:51:19 +0800

    refactor: change crate to proc-macro

--- examples/my_ready.rs
+++ examples/my_ready.rs
@@ -1,4 +1,3 @@
-use macros::my_ready;
 use std::{
     future::Future,
     pin::Pin,
@@ -43,4 +42,15 @@ impl Future for MyFut {
             Poll::Pending
         }
     }
+}
+
+// my_ready! => Poll::Ready / Poll::Pending
+#[macro_export]
+macro_rules! my_ready {
+    ($expr:expr) => {
+        match $expr {
+            std::task::Poll::Ready(v) => std::task::Poll::Ready(v),
+            std::task::Poll::Pending => return std::task::Poll::Pending,
+        }
+    };
 }
